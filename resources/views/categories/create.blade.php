708135236eafb28ff68b0366fb9b97ca34a1165d
IqbalFirmans <iqbalclan74@gmail.com>
2024-07-12 15:00:47 +0700

    up style category

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,35 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Category</title>
-</head>
-<body>
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <ul>
-                <li>{{ $error }}</li>
-            </ul>
-        @endforeach
-    @endif
+@extends('layouts.main')
 
-    <h3>Tambah Category</h3>
-    <form action="{{ route('categories.store') }}" method="post">
-        @csrf
+@section('title', 'Create Category')
+@section('content')
 
-        <div>
-            <label for="name">Name : </label>
-            <input type="text" name="name">
+    <section class="bg-gray-100">
+        <div class="mx-auto max-w-screen-xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+
+                <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                    <form action="{{ route('categories.store') }}" class="space-y-4" method="POST">
+                        @csrf
+                        <div>
+                            <label class="sr-only" for="name">Name</label>
+                            <input class="w-full rounded-lg border-red-500 p-3 text-sm" placeholder="Name" type="text"
+                                id="name" name="name" value="{{ old('name') }}" />
+
+                            @error('name')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label class="sr-only" for="slug">Slug</label>
+                            <input class="w-full rounded-lg border-gray-200 p-3 text-sm" placeholder="Slug" type="text"
+                                id="slug" name="slug"/>
+
+                            @error('slug')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mt-4 flex justify-end gap-3">
+                            <a href="/categories" class="inline-block w-full rounded-lg bg-red-600 px-5 py-3 font-medium text-white sm:w-auto">Back</a>
+                            <button type="submit"
+                                class="inline-block w-full rounded-lg bg-indigo-600 px-5 py-3 font-medium text-white sm:w-auto">
+                                Create
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="slug">Slug : </label>
-            <input type="text" name="slug">
-        </div>
+    </section>
 
-        <a href="{{ route('categories.index') }}">Back</a>
-        <button type="submit">Simpan</button>
-    </form>
-</body>
-</html>
+@endsection
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,47 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index Categories</title>
-</head>
-<body>
-    <h3>Data Category</h3>
-    <a href="{{ route('categories.create') }}"><button>Tambah</button></a>
-    <table>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Slug</th>
-                <th>Total Menu</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($categories as $category)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $category->name }}</td>
-                    <td>{{ $category->slug }}</td>
-                    <td>{{ $category->menus_count }}</td>
-                    <td>
-                        <a href="{{ route('categories.edit', $category->id) }}"><button>Edit</button></a>
-                        <form action="{{ route('categories.destroy', $category->id) }}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button>Hapus</button>
-                        </form>
-                    </td>
+@extends('layouts.main')
 
-                </tr>
-            @empty
-                <tr>
-                    <td>Tidak ada data!</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
-</body>
-</html>
+@section('title', 'Categories')
+
+@section('content')
+    <header>
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <div class="text-center sm:text-left">
+                    <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">All Categories!</h1>
+
+                    <p class="mt-1.5 text-sm text-gray-500">Let's write a new categories menu! 🎉</p>
+                </div>
+
+                <div class="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+                    <a
+                        class="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+                        href="{{ route('categories.create') }}">
+                        Create Post
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table id="example" class="w-full text-sm text-left rtl:text-right text-light-500 dark:text-light-400">
+                <thead class="text-xs text-light-700 uppercase bg-light-50 dark:bg-light-700 dark:text-light-400">
+                    <tr>
+
+                        <th scope="col" class="px-6 py-3">No</th>
+                        <th scope="col" class="px-6 py-3">Category Name</th>
+                        <th scope="col" class="px-6 py-3">Menu Count</th>
+                        <th scope="col" class="px-6 py-3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr class="bg-white border-b dark:bg-light-800 dark:border-light-700">
+                            <td class="px-6 py-4">1</td>
+                            <th scope="row" class="px-6 py-4 font-medium text-dark-900 whitespace-nowrap dark:text-dark">
+                                {{ $category->name }}
+                            </th>
+                            <td class="px-6 py-4">{{ $category->menus_count }}</td>
+                            <td class="px-6 py-4">
+                                <a href="#"
+                                    class="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
+                                    View
+                                </a>
+                                <a href="{{ route('categories.edit', $category->id) }}"
+                                    class="inline-block rounded bg-yellow-600 px-4 py-2 text-xs font-medium text-white hover:bg-yellow-700">
+                                    Edit
+                                </a>
+
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="post"
+                                    class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+@endsection
